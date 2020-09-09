86f0a5fe334f28666df9707f588e0173f8f7a3bc
Catalina Duplat <Catalina.DuplatMolano@adidas.com>
2020-09-08 21:29:53 -0500

    Experiment for subject 1

--- firstExperiment.py
+++ firstExperiment.py
@@ -1,14 +1,30 @@
 import SimpleITK as sitk
 
-fixedImage = sitk.ReadImage('data/FirstExperiment/sub-2117-T1.nii')
-movingImage = sitk.ReadImage('data/FirstExperiment/sub-2117-T2.nii')
-parameterMap = sitk.GetDefaultParameterMap('translation')
+
+fixed_image = sitk.ReadImage('data/FirstExperiment/Normal001-DTI.mha', sitk.sitkFloat32)
+fixedImage2d = sitk.Cast(fixed_image, sitk.sitkFloat32)
+# fixedImage2d = sitk.Extract(fixedImage3d, (fixedImage3d.GetWidth(), fixedImage3d.GetHeight(), 0), (0, 0, 0))
+
+moving_image = sitk.ReadImage('data/FirstExperiment/Normal001-MRA.mha', sitk.sitkFloat32)
+# movingImage2d = sitk.Extract(movingImage3d, (movingImage3d.GetWidth(), movingImage3d.GetHeight(), 0), (0, 0, 0))
+
+movingImage2d = sitk.Cast(moving_image, sitk.sitkFloat32)
+
+# initial_transform = sitk.CenteredTransformInitializer(fixed_image, 
+#                                                       moving_image, 
+#                                                       sitk.Euler2DTransform(), 
+#                                                       sitk.CenteredTransformInitializerFilter.GEOMETRY)
+
+parameterMap = sitk.GetDefaultParameterMap('affine')
+parameterMap["FixedImagePyramid"] = ["FixedShrinkingImagePyramid"] 
+parameterMap["MovingImagePyramid"] = ["MovingShrinkingImagePyramid"] 
 
 elastixImageFilter = sitk.ElastixImageFilter()
-elastixImageFilter.SetFixedImage(fixedImage)
-elastixImageFilter.SetMovingImage(movingImage)
+elastixImageFilter.SetFixedImage(fixedImage2d)
+elastixImageFilter.SetMovingImage(movingImage2d)
 elastixImageFilter.SetParameterMap(parameterMap)
 elastixImageFilter.Execute()
 
 resultImage = elastixImageFilter.GetResultImage()
+sitk.WriteImage(resultImage, "resultImage2117.nii")
 transformParameterMap = elastixImageFilter.GetTransformParameterMap()
