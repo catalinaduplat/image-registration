87190cacb40b99582d0b3056a322d1a43865aa0a
Catalina Duplat <Catalina.DuplatMolano@adidas.com>
2021-04-14 21:13:02 -0500

    Create file for comparing segmentations

---
+++ comp-segmentation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  7 15:35:43 2021
+
+@author: duplacat
+"""
+
+import os
+import SimpleITK as sitk
+import time
+import sys
+import re
+
+
+if len ( sys.argv ) < 3:
+    print( "usage: {0} <originalImage> <registrationImage>".format(sys.argv[0]))
+    sys.exit ( 1 )
+
+OUTPUT_DIR = 'Segmentation/Output'
+originalImage = sitk.ReadImage(sys.argv[1], sitk.sitkFloat32)
+registrationImage = sitk.ReadImage(sys.argv[2], sitk.sitkFloat32)
+checkerboard = sitk.CheckerBoard(originalImage, registrationImage, (20,20,20))
+        
+sitk.WriteImage(checkerboard, os.path.join(OUTPUT_DIR, "comp-segmentation.nii"))
